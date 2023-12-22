e73f69bf8033c69e88aed62bc9478711e9fd53b7
Joris <joris@guyonvarch.me>
2023-12-22 17:29:39 +0100

    Migrate to Qt6

--- kiosk/default.nix
+++ kiosk/default.nix
@@ -15,32 +15,37 @@ python3Packages.buildPythonApplication rec {
       --replace "@system_version@" "${system_version}"
   '';
 
-  doCheck = false;
-
-  nativeBuildInputs = [ qt5.wrapQtAppsHook mypy ];
-
   propagatedBuildInputs = with python3Packages; [
     dbus-python
     pygobject3
-    pyqtwebengine
+    pyqt6
+    pyqt6-webengine
     pytest
+    qt6.full
+    qt6.qtbase
+    qt6.qtwebengine
     requests
     types-requests
+  ];
+
+  nativeBuildInputs = [
+    bashInteractive
+    makeWrapper
+    mypy
+    qt6.wrapQtAppsHook
   ];
 
   postInstall = ''
     cp -r images/ $out/images
   '';
 
-  dontWrapQtApps = true;
-  makeWrapperArgs = [ "\${qtWrapperArgs[@]}" ];
-
   shellHook = ''
     # Give access to kiosk_browser module
     export PYTHONPATH=./:$PYTHONPATH
 
-    # Give access to Qt platform plugin "xcb" in nix-shell
-    export QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
+    export QT_QPA_PLATFORM=wayland
+    bashdir=$(mktemp -d)
+    makeWrapper "$(type -p bash)" "$bashdir/bash" "''${qtWrapperArgs[@]}"
+    exec "$bashdir/bash"
   '';
-
 }
